fd1f077a238667f9c03a6e5c478f1f61294c069f
petr0n <petron@gmail.com>
2019-04-17 20:46:50 -0500

    fix routes and links

--- server.js
+++ server.js
@@ -14,62 +14,59 @@ app.use(express.json());
 
 // Routes
 // =============================================================
+// HTML ROUTES
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/reservationform", function (req, res) {
-    res.sendFile(path.join(__dirname, "reservationForm.html"));
+app.get("/addreservation", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
 });
 
-app.get("/api/tables", function(req, res) {
-    return res.json();
-  });
 
-  app.get("/api/reservations", function(req, res) {
-    return getReservations ();
-  });  
+// API ROUTES
+app.get("/api/tables", function (req, res) {
+    return res.json();
+});
+
+app.get("/api/reservations", function (req, res) {
+    return getReservations();
+});
 
 app.post("/api/makereservation", function (req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newParty = req.body;
-
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newParty.routeName = newParty.name.replace(/\s+/g, "").toLowerCase();
-
     console.log(newParty);
 
-    tables.push(newParty)
+    // // Using a RegEx Pattern to remove spaces from newCharacter
+    // // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // newParty.routeName = newParty.name.replace(/\s+/g, "").toLowerCase();
 
-    res.json(newParty);
+    // console.log(newParty);
+
+    // tables.push(newParty)
+
+    // res.json(newParty);
 });
-
-// home
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-
 
 
 // Functions
 // =============================================================
-function getTables(){
+function getTables() {
     let tablesJSON = fs.readFileSync(tableFile, 'utf8');
     tables = JSON.parse(tablesJSON);
     return tables;
 }
-function getWaiting(){
+function getWaiting() {
     let tables = getTables();
     return tables.filter(t => !t.hasReservation)
 }
-function getReservations(){
+function getReservations() {
     let tables = getTables();
     return tables.filter(t => t.hasReservation);
 }
@@ -77,10 +74,10 @@ getTables();
 // console.log(getWaiting());
 // console.log(getReservations());
 
-function addTable(){
+function addTable(newParty) {
     let tables = getTables();
     let tableCount = Object.keys(tables).length;
-    
+
     // fs.writeFile(tableFile, table, function(error){
     //     if (error) { console.log(error) }
     // });
@@ -90,8 +87,7 @@ addTable();
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-  
-  
+});
+
